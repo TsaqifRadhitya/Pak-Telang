f1bd5b2646da813314afba2704c8ff4e3ca3801b
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2025-04-01 14:22:40 +0700

    Finish Product Detail Page

--- resources/js/pages/Guest/Produk/Components/productDetailSection.tsx
+++ resources/js/pages/Guest/Produk/Components/productDetailSection.tsx
@@ -1,11 +1,52 @@
-import { usePage } from "@inertiajs/react";
-import { props } from "../produkDetail";
+import Heading from '@/components/heading';
+import HeadingSmall from '@/components/heading-small';
+import { Button } from '@/components/ui/button';
+import { usePage } from '@inertiajs/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { props } from '../produkDetail';
 
-export default function productDetailSection(){
-    const {productDetail} = usePage<props>().props
-    return(<section className="w-full h-screen bg-[#EBEFFF] p-5 lg:px-10 lg:py-20 lg:pb-10">
-        <main className="bg-[#FFFFFF] w-full h-full shadow-md rounded-lg border border-[#AFB3FF]">
-
-        </main>
-    </section>)
+export default function productDetailSection() {
+    const { productDetail } = usePage<props>().props;
+    return (
+        <section className="h-screen w-full bg-[#EBEFFF] p-5 pt-24 lg:px-10 lg:py-20 lg:pt-20 lg:pb-10">
+            <main className="flex h-full w-full flex-col gap-5 rounded-lg border border-[#AFB3FF] bg-[#FFFFFF] p-7 shadow-md lg:flex-row lg:gap-20 lg:p-10">
+                <article className="relative flex aspect-8/9 flex-1/3 overflow-y-auto rounded-lg bg-[#AFB3FF] shadow">
+                    <Swiper
+                        navigation
+                        pagination={{ clickable: true }}
+                        modules={[Navigation]}
+                        className="h-auto w-full max-w-sm overflow-hidden rounded-lg"
+                    >
+                        {productDetail.productPhoto.map((photo, index) => (
+                            <SwiperSlide key={index} className="relative">
+                                <img src={photo} alt={`Product ${index + 1}`} className="h-auto w-full object-contain absolute top-1/2 left-1/2 -translate-1/2" />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </article>
+                <article className="relative flex flex-2/3 flex-col gap-y-5 py-2.5 lg:gap-y-10">
+                    <section className="flex flex-col gap-y-2.5 lg:gap-y-5">
+                        <Heading title={productDetail.productName} disableMb className="text-2xl text-[#3B387E] lg:text-4xl" />
+                        <h1 className="text-2xl font-bold text-[#FFAC31] lg:text-4xl">
+                            {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(productDetail.productPrice)}
+                        </h1>
+                    </section>
+                    <section className="flex flex-col gap-y-2.5">
+                        <HeadingSmall title="Deskripsi:" className="text-xl text-[#3B387E]" />
+                        <p className="text-[#5961BE] lg:text-xl">{productDetail.productDescription}</p>
+                        <p className="text-[#3B387E] lg:text-xl">
+                            Netto: {productDetail.productNetto} {productDetail.productUnit}
+                        </p>
+                    </section>
+                    <Button className="absolute right-0 bottom-0 min-h-14 w-full cursor-pointer rounded-3xl bg-[#5961BE] text-2xl text-white hover:bg-[#424a9e] lg:w-1/4">
+                        <p>Beli Sekarang</p>
+                    </Button>
+                </article>
+            </main>
+        </section>
+    );
 }
--- resources/js/pages/Guest/Produk/Components/productLainnyaSection.tsx
+++ resources/js/pages/Guest/Produk/Components/productLainnyaSection.tsx
@@ -1,11 +1,52 @@
-import { usePage } from "@inertiajs/react";
-import { props } from "../produkDetail";
+import { Button } from '@/components/ui/button';
+import { router, usePage } from '@inertiajs/react';
+import Heading from '../../../../components/heading';
+import HeadingSmall from '../../../../components/heading-small';
+import { props } from '../produkDetail';
 
-export default function productLainnyaSection(){
-    const {products} = usePage<props>().props
-    return(<section className="w-full h-screen bg-[#EBEFFF] p-5 lg:px-10 lg:pt-10 lg:pb-20">
-        <main className="w-full h-full bg-[#FFFFFF] border border-[#AFB3FF] shadow-lg rounded-lg">
-
-        </main>
-    </section>)
+export default function productLainnyaSection() {
+    const { products } = usePage<props>().props;
+    return (
+        <section className="min-h-screen w-full bg-[#EBEFFF] p-5 lg:px-10 lg:pt-10 lg:pb-20">
+            <main className="flex h-full w-full flex-col gap-y-10 rounded-lg border border-[#AFB3FF] bg-[#FFFFFF] p-7 lg:-10 shadow-lg">
+                <header className="flex flex-col gap-y-1.5">
+                    <Heading title="Produk Lainnya" disableMb className="text-4xl text-[#3B387E]" />
+                    <HeadingSmall title="Mungkin anda suka" className="text-[#3B387E]" />
+                </header>
+                <article className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 xl:grid-cols-3 md:gap-28">
+                    {products.map((item) => (
+                        <section
+                            onClick={(e) => {
+                                if (!(e.target as HTMLElement).closest('button')) {
+                                    router.get(route('produk.detail', { id: item.id }));
+                                }
+                            }}
+                            className="relative aspect-2/3 w-full rounded-lg bg-[#EBEFFF] shadow cursor-pointer pb-2.5"
+                        >
+                            <div className="h-2/5 w-full rounded-t-lg bg-[#9A9FFF]"></div>
+                            <div className="absolute top-0 left-1/2 h-1/2 w-4/5 -translate-x-1/2 rounded-b-full bg-linear-180 from-[#9A9FFF] to-[#7D85DE]">
+                                <img
+                                    src={item.productPhoto[0]}
+                                    alt=""
+                                    className="absolute bottom-0 left-1/2 aspect-6/9 w-1/2 -translate-x-1/2 object-cover object-center"
+                                />
+                            </div>
+                            <div className="mt-20 w-3/5 rounded-r-full bg-[#5961BE] px-6 py-2 shadow-md">
+                                <h1 className="text-md w-fit pr-4 font-medium text-white md:text-xl 2xl:text-xl">{item.productName}</h1>
+                            </div>
+                            <p className="line-clamp-5 px-6 py-6 text-sm font-medium text-[#5961BE]">{item.productDescription}</p>
+                            <div className="mt-auto flex w-full items-center justify-between px-6">
+                                <h1 className="text-md font-bold text-[#5961BE] md:text-xl">
+                                    {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.productPrice)}
+                                </h1>
+                                <Button className="cursor-pointer rounded-full bg-transparent text-[#5961BE] ring-2 ring-[#5961BE] hover:bg-[#5961BE] hover:text-white">
+                                    Beli Sekarang
+                                </Button>
+                            </div>
+                        </section>
+                    ))}
+                </article>
+            </main>
+        </section>
+    );
 }
