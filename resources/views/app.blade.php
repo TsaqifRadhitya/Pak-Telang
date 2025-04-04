851eeec45829c60621a1eb743b61c635b1762bda
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2025-04-04 22:31:10 +0700

    Sprint 1 fix Error Message on Register Page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     {{-- Inline script to detect system dark mode preference and apply it immediately --}}
     <script>
         (function() {
