1bf5ca115dc4a868f8bc9813aa591a380631b7c8
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2025-05-22 13:09:49 +0700

    Sprint 4 add room chat ui

--- resources/js/components/chat/roomChat.tsx
+++ resources/js/components/chat/roomChat.tsx
@@ -1,11 +1,21 @@
 import { messageType } from '@/pages/chat/roomChat';
 import { User } from '@/types';
+import { useRef, useState } from 'react';
+import Heading from '../heading';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
 
-export default function RoomChat({ sender, receiver, messages }: { sender: User; receiver: User; messages: messageType[] }) {
+export default function RoomChat({ user, target, messages }: { user: User; target: User; messages: messageType[] }) {
     // const [messagesState, setMessages] = useState<messageType[]>(messages);
+    const [image, setImage] = useState<File>();
+    const inputFile = useRef<HTMLInputElement>(null);
 
-    console.log(sender, receiver, messages);
-
+    const handleChangeInputImage = (param: FileList | null) => {
+        if (param?.length && param.length > 0) {
+            setImage(Object.values(param)[0]);
+        }
+    };
     // const handleSendMessage = () => {
 
     // }
@@ -23,6 +33,61 @@ export default function RoomChat({ sender, receiver, messages }: { sender: User;
     //     const newMessages = [...messagesState].filter((message) => message.id != id)
     //     setMessages(newMessages)
     // }
-
-    return <></>;
+    console.log(user,messages)
+    return (
+        <section className="flex flex-1 flex-col p-10 pb-5">
+            <div className="flex items-center gap-5 border-b-2 border-[#D9D9D9] px-2 pb-2.5">
+                <img src={target.profile_picture} className="aspect-square w-12 rounded-full object-cover object-center" alt="" />
+                <Heading title={target.name} />
+            </div>
+            <div className="flex-1">{image && <img alt="" src={image && URL.createObjectURL(image)} />}</div>
+            <form action="" className="relative mt-1 flex gap-2.5">
+                <Textarea
+                    placeholder="Ketikan pesan...."
+                    className="h-10 min-h-0 flex-1 border-0 pr-7 text-[#3B387E] ring ring-[#3B387E] placeholder:text-[#3B387E] focus-visible:ring-3 focus-visible:ring-[#3B387E]"
+                />
+                <Input
+                    type="file"
+                    className="hidden"
+                    ref={inputFile}
+                    accept="image/png, image/jpeg"
+                    onChange={(e) => handleChangeInputImage(e.target.files)}
+                />
+                <svg
+                    className="absolute top-1/2 right-16 -translate-y-1/2 cursor-pointer"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    onClick={() => inputFile.current?.click()}
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <g clipPath="url(#clip0_1378_6370)">
+                        <path
+                            d="M19 7V9.99C19 9.99 17.01 10 17 9.99V7H14C14 7 14.01 5.01 14 5H17V2H19V5H22V7H19ZM16 11V8H13V5H5C3.9 5 3 5.9 3 7V19C3 20.1 3.9 21 5 21H17C18.1 21 19 20.1 19 19V11H16ZM5 19L8 15L10 18L13 14L17 19H5Z"
+                            fill="#3B387E"
+                        />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_1378_6370">
+                            <rect width="24" height="24" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+                <Button type="submit" className="group h-10 cursor-pointer ring ring-[#3B387E] hover:bg-[#3B387E]">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clipPath="url(#clip0_1378_6373)">
+                            <path d="M15.5 5H11L16 12L11 19H15.5L20.5 12L15.5 5Z" className="fill-[#3B387E] group-hover:fill-white" />
+                            <path d="M8.5 5H4L9 12L4 19H8.5L13.5 12L8.5 5Z" className="fill-[#3B387E] group-hover:fill-white" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_1378_6373">
+                                <rect width="24" height="24" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </Button>
+            </form>
+        </section>
+    );
 }
--- resources/js/pages/Mitra/Chat/create.tsx
+++ resources/js/pages/Mitra/Chat/create.tsx
@@ -13,12 +13,12 @@ export default function Create() {
     const { auth, receiver, messages } = usePage<props>().props;
     console.log(messages);
     return (
-        <MitraPageLayout page='Bantuan'>
+        <MitraPageLayout page="Bantuan">
             <main className="flex h-full w-full flex-col rounded-t-lg border-[1px] border-b-0 border-[#AFB3FF] bg-[#FFFFFF] shadow-lg">
                 <div className="flex items-center border-b-[1px] border-[#AFB3FF] p-5 md:px-10 md:py-5">
                     <h1 className="text-xl font-semibold">Chat</h1>
                 </div>
-                <RoomChat sender={auth.user} receiver={receiver} messages={messages}/>
+                <RoomChat user={auth.user} target={receiver} messages={messages} />
             </main>
         </MitraPageLayout>
     );
